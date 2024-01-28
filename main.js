04f554fb410c3116cd4cd02b0b32ade6d597f686
satyamgupta0704 <21bcs7408@cuchd.in>
2024-01-29 01:43:06 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+// const person={
+//     name:"Satyam",
+//     UID:7408,
+//     isStudent:true,
+//     skills:["c++", "dsa", 420]
+
+// };
+// console.log(person.name);
+// console.log(person.UID);
+// console.log(person.skills);
+// const ages=[33, 12, 87, 17, 35]
+// const result= ages.filter(checkAge);
+// function checkAge(ages){
+//     return ages<=18;
+// }
+// console.log(result);
+
